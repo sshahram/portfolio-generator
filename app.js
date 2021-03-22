f7f301f6b2f9c00b39900506cef0a449a1297b97
sshahram <shirin.shahram23@gmail.com>
2021-03-22 14:42:49 -0700

    create html file

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr => {
-    // This ...
-   for (let i = 0; i < profileDataArr.length; i++) {
-       console.log(profileDataArr[i]);
-   }
+const fs = require('fs');
+const generatePage = require('./src/page-template.js'); 
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-   console.log("======================")
+// const printProfileData = profileDataArr => {
+//     // This ...
+//    for (let i = 0; i < profileDataArr.length; i++) {
+//        console.log(profileDataArr[i]);
+//    }
 
-   // Is the same as this ...
-   profileDataArr.forEach(profileItem =>  console.log(profileItem));
-};
+//    console.log("======================")
 
-printProfileData(profileDataArgs);
+//    // Is the same as this ...
+//    profileDataArr.forEach(profileItem =>  console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if(err) throw new Error(err);
+
+    console.log('Portfolio complete! check out index.html to see the output!');
+});
