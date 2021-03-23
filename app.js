11dcd2b2f4a8c195f8e850eabaf8e416e64c0dcb
sshahram <shirin.shahram23@gmail.com>
2021-03-23 00:21:54 -0700

    get user's name

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js'); 
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js'); 
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
-
-// const printProfileData = profileDataArr => {
-//     // This ...
-//    for (let i = 0; i < profileDataArr.length; i++) {
-//        console.log(profileDataArr[i]);
-//    }
-
-//    console.log("======================")
-
-//    // Is the same as this ...
-//    profileDataArr.forEach(profileItem =>  console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
+// const pageHTML = generatePage(name, github);
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if(err) throw new Error(err);
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if(err) throw new Error(err);
 
-    console.log('Portfolio complete! check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! check out index.html to see the output!');
+// });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
